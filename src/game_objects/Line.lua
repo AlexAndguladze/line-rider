dadfc72febdf70692db5b084b91454cf48407996
FASTPAY\a.andghuladze <andghuladze.alex@gmail.com>
2024-11-07 13:15:07 +0400

    add draw bottom line

--- src/game_objects/Line.lua
+++ src/game_objects/Line.lua
@@ -61,6 +61,22 @@ function Line:draw()
    end
    lg.setLineWidth(4)
    lg.line(self.x1, self.y1, self.x2, self.y2)
+
+   local dx, dy = self.x2 - self.x1, self.y2 - self.y1
+   local offset = 4
+   local length = math.sqrt(dx^2 + dy^2)
+   local perp_x = -dy / length
+   local perp_y = dx / length
+   
+   local bottom_x1 = self.x1 + perp_x * offset
+   local bottom_y1 = self.y1 + perp_y * offset
+   local bottom_x2 = self.x2 + perp_x * offset
+   local bottom_y2 = self.y2 + perp_y * offset
+
+   lg.setColor(0.5, 0.5, 1)
+   lg.setLineWidth(2)
+   lg.line(bottom_x1, bottom_y1, bottom_x2, bottom_y2)
+
 end
 
 function Line:debug()
