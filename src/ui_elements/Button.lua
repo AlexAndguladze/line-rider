ac37bb51492411eae92dcb2b892027f4182bcf64
FASTPAY\a.andghuladze <andghuladze.alex@gmail.com>
2024-12-11 17:09:20 +0400

    finish on click and on mouse down logic for ui button

--- src/ui_elements/Button.lua
+++ src/ui_elements/Button.lua
@@ -3,10 +3,12 @@ local Button = Class:extend({
    y = 0,
    w = 10,
    h = 10,
-   
+   on_click = nil,
+   on_mouse_down = nil,
    mouse_btn = 1,
    is_down = false,
 })
+
 function Button:is_hovered()
    local mx, my = love.mouse.getPosition()
    return mx >= self.x and mx < self.x + self.w and my >= self.y and my < self.y + self.h
@@ -14,23 +16,22 @@ end
 
 function Button:update()
    -- button click once
+  
    if self.is_down == false and love.mouse.isDown(self.mouse_btn) and self:is_hovered() then
       self.is_down = true
    elseif self.is_down == true and not love.mouse.isDown(self.mouse_btn) then
       self.is_down = false
-      if self:is_hovered() then -- not call if mouse moved away
-         self:on_click()
+      if self.on_click and self:is_hovered() then -- call if only mouse is still hovered above
+         self.on_click()
       end
    end
-   
-end
 
-function Button:on_click()
-   print("Clicked once")
+   if self.on_mouse_down then
+      if self.is_down == true and love.mouse.isDown(self.mouse_btn) and self:is_hovered() then
+         self.on_mouse_down()
+      end
+   end
 end
--- function Button:on_mouse_down()
---    print("Click continuously")
--- end
 
 function Button:draw()
    lg.setColor(1, 0.5, 1)
